5d30af7917f68cda873b861286baaebf8db2f279
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-23 23:32:34 -0400

    feat: Fecth the Pokemon data

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=100").then((response) => response.json()).then((data) => {
+      setPokemons(data.results)
+    }).catch((error) => console.log(error));
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Pokedex</Text>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}><Text>{pokemon.name}</Text></View>
+      ))}
+    </ScrollView>
   );
 }
