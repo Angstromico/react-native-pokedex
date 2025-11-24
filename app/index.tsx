6cf74771734d3e5d297d6edcb3e340aa8926220d
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-24 00:05:55 -0400

    feat: Two columns of pokemons

--- app/index.tsx
+++ app/index.tsx
@@ -21,9 +21,20 @@ export default function Index() {
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=100").then((response) => response.json()).then(async (data) => {
       const detailedPokemons: PokemonDetails[] = await Promise.all(
-          data.results.map(async (pokemon: any) => {
+          data.results.map(async (pokemon: Pokemon) => {
             const res = await fetch(pokemon.url);
-            return await res.json();
+            const pokemonDetails = await res.json();
+
+            console.log(pokemonDetails);
+
+            /*
+            
+              Each element of the details has cries 
+              
+
+            */
+
+            return pokemonDetails;
           })
         );
       setPokemons(detailedPokemons);
@@ -32,39 +43,53 @@ export default function Index() {
 
   return (
     <ScrollView style={{ padding: 16 }}>
+    <View
+      style={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+      }}
+    >
       {pokemons.map((pokemon) => (
         <View
           key={pokemon.id}
           style={{
+            width: "48%", // 2-column responsive grid
             backgroundColor: "#f2f2f2",
             borderRadius: 12,
-            padding: 16,
+            padding: 12,
             marginBottom: 16,
-            flexDirection: "row",
             alignItems: "center",
           }}
         >
           {/* Pokémon Image */}
           <Image
             source={{ uri: pokemon.sprites.front_default }}
-            style={{ width: 80, height: 80, marginRight: 16 }}
+            style={{ width: 90, height: 90, marginBottom: 8 }}
+            resizeMode="contain"
           />
 
           {/* Pokémon Info */}
-          <View>
-            <Text style={{ fontSize: 20, fontWeight: "bold", textTransform: "capitalize" }}>
-              {pokemon.name}
-            </Text>
-            <Text>Height: {pokemon.height}</Text>
-            <Text>Weight: {pokemon.weight}</Text>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: "bold",
+              textTransform: "capitalize",
+              marginBottom: 4,
+            }}
+          >
+            {pokemon.name}
+          </Text>
 
-            <Text>
-              Type:{" "}
-              {pokemon.types.map((t) => t.type.name).join(", ")}
-            </Text>
-          </View>
+          <Text style={{ fontSize: 12 }}>Height: {pokemon.height}</Text>
+          <Text style={{ fontSize: 12 }}>Weight: {pokemon.weight}</Text>
+
+          <Text style={{ fontSize: 12, marginTop: 4 }}>
+            Type: {pokemon.types.map((t) => t.type.name).join(", ")}
+          </Text>
         </View>
       ))}
-    </ScrollView>
+    </View>
+  </ScrollView>
   );
 }
