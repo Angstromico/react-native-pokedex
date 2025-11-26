2e7e628fe0149e60b6e013c2571817622aa4e08d
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-26 13:50:37 -0400

    feat: headerShown: false

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  headerShown: false,
+};
+
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
